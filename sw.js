1e95e8bc42e5e1986b73ae1b5cb442296217c956
BorsosIstvan <bistvanus@gmail.com>
2026-07-25 20:04:47 +0200

    pwa

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'hitjamparty-v1';
+const ASSETS = [
+  '.',
+  'index.html',
+  'manifest.json'
+];
+
+// Bestanden cachen bij installatie
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Bestanden serveren vanuit de cache wanneer offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
